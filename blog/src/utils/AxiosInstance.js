961c091644c12290cf59bf00fcc95f161bd21023
gratien <gracian2020@gmail.com>
2022-10-06 06:59:14 +0200

    added production url

--- blog/src/utils/AxiosInstance.js
+++ blog/src/utils/AxiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const isDev = process.env.NODE_ENV === "development";
 
 const instance = axios.create({
-  baseURL: isDev ? "http://localhost:3000/api" : "https://gratien.vercel.app",
+  baseURL: isDev ? "http://localhost:3000/api" : "https://next-blog-ivory-six.vercel.app/api",
 });
 
 export default instance;
